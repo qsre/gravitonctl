772e9ae8ed09a57b243f42ae0e20ffdbf37892d2
Mathis Van Eetvelde <mathis.vaneetvelde@protonmail.com>
2022-01-02 02:13:18 +0100

    Add better support for SecurityGroups
    Signed-off-by: Mathis Van Eetvelde <mathis.vaneetvelde@protonmail.com>

--- pkg/aws/start.go
+++ pkg/aws/start.go
@@ -2,6 +2,7 @@ package aws
 
 import (
 	"errors"
+	"fmt"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/awserr"
@@ -22,6 +23,91 @@ func Start(name string, keyName string) error {
 		return errors.New("this instance already exists")
 	}
 
+	// Security Groups
+	// TODO: needs to be in gravitonctl/pkg/aws
+
+	sgName := fmt.Sprintf("%s-sg", name)
+	var sg ec2.SecurityGroup
+
+	sgOutput, _ := ec2svc.DescribeSecurityGroups(&ec2.DescribeSecurityGroupsInput{
+		GroupNames: []*string{
+			&sgName,
+		},
+	})
+
+	if sgOutput.SecurityGroups == nil {
+		_, err := ec2svc.CreateSecurityGroup(&ec2.CreateSecurityGroupInput{
+			Description: &sgName,
+			GroupName:   &sgName,
+		})
+		if err != nil {
+			return err
+		}
+
+		err = ec2svc.WaitUntilSecurityGroupExists(&ec2.DescribeSecurityGroupsInput{
+			GroupNames: []*string{
+				&sgName,
+			},
+		})
+		if err != nil {
+			return err
+		}
+
+		describeSgOutput, err := ec2svc.DescribeSecurityGroups(&ec2.DescribeSecurityGroupsInput{
+			GroupNames: []*string{
+				&sgName,
+			},
+		})
+
+		if err != nil {
+			return err
+		}
+
+		sg = *describeSgOutput.SecurityGroups[0]
+
+		_, err = ec2svc.AuthorizeSecurityGroupIngress(&ec2.AuthorizeSecurityGroupIngressInput{
+			GroupId: describeSgOutput.SecurityGroups[0].GroupId,
+			IpPermissions: []*ec2.IpPermission{
+				{
+					FromPort:   aws.Int64(22),
+					ToPort:     aws.Int64(22),
+					IpProtocol: aws.String("tcp"),
+					IpRanges: []*ec2.IpRange{
+						{
+							CidrIp:      aws.String("0.0.0.0/0"),
+							Description: aws.String("Inbound ssh access"),
+						},
+					},
+				},
+			},
+		})
+		if err != nil {
+			return err
+		}
+
+		// outbound
+		_, err = ec2svc.UpdateSecurityGroupRuleDescriptionsEgress(&ec2.UpdateSecurityGroupRuleDescriptionsEgressInput{
+			GroupId: describeSgOutput.SecurityGroups[0].GroupId,
+			IpPermissions: []*ec2.IpPermission{
+				{
+					IpProtocol: aws.String("-1"),
+					IpRanges: []*ec2.IpRange{
+						{
+							CidrIp:      aws.String("0.0.0.0/0"),
+							Description: aws.String("Outbound access"),
+						},
+					},
+				},
+			},
+		})
+		if err != nil {
+			return err
+		}
+
+	} else {
+		sg = *sgOutput.SecurityGroups[0]
+	}
+
 	// bare minimum input
 	input := &ec2.RunInstancesInput{
 		BlockDeviceMappings: []*ec2.BlockDeviceMapping{
@@ -37,6 +123,10 @@ func Start(name string, keyName string) error {
 		KeyName:      &keyName,
 		MaxCount:     aws.Int64(1),
 		MinCount:     aws.Int64(1),
+
+		SecurityGroupIds: []*string{
+			sg.GroupId,
+		},
 
 		// SecurityGroupIds: []*string{
 		// 	aws.String("sg-064d01f01ebe545ce"),
